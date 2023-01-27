b8d5899273842741b3d92284d3157f1b111b14a0
Rafael Rocha <r.rocha988@hotmail.com>
2023-01-26 21:47:06 -0300

    Validação do cadastro/1

--- cadastro.js
+++ cadastro.js
@@ -1,5 +1,96 @@
 let btn = document.querySelector('#verSenha')
 
+
+let nome = document.querySelector('#nome')
+let labelNome = document.querySelector('#labelNome')
+let validNome = false
+
+let usuario = document.querySelector('#usuario')
+let labelusuario = document.querySelector('#labelusuario')
+let validUsuario = false
+
+let senha = document.querySelector('#senha')
+let labelSenha = document.querySelector('#labelSenha')
+let validSenha = false
+
+let confirmeSenha = document.querySelector('#confirmeSenha')
+let labelConfirmeSenha = document.querySelector('#labelConfirmeSenha')
+let validConfirmeSenha = false
+
+nome.addEventListener('keyup', () => {
+    if(nome.value.length <= 3) {
+        labelNome.setAttribute('style', 'color: red')
+        labelNome.innerHTML = ' Nome: Min 4 Caracteres '
+        nome.setAttribute('style', 'border-color:red')
+        validNome = false
+
+    }else{
+        labelNome.setAttribute('style', 'color: white')
+        labelNome.innerHTML = 'Nome'
+        nome.setAttribute('style', 'border-color:white')
+        validNome = true
+
+    }
+})
+
+usuario.addEventListener('keyup', () => {
+    if(usuario.value.length <= 4) {
+        labelusuario.setAttribute('style', 'color: red')
+        labelusuario.innerHTML = ' Usuário: Min 5 Caracteres '
+        usuario.setAttribute('style', 'border-color:red')
+        validUsuario = false
+
+    }else{
+        labelusuario.setAttribute('style', 'color: white')
+        labelusuario.innerHTML = 'Usuário'
+        usuario.setAttribute('style', 'border-color:white')
+        validUsuario = true
+
+    }
+})
+
+senha.addEventListener('keyup', () => {
+    if(senha.value.length <= 5) {
+        labelSenha.setAttribute('style', 'color: red')
+        labelSenha.innerHTML = ' Senha: Min 6 Caracteres '
+        senha.setAttribute('style', 'border-color:red')
+        validSenha = false
+
+    }else{
+        labelSenha.setAttribute('style', 'color: white')
+        labelSenha.innerHTML = 'Senha'
+        senha.setAttribute('style', 'border-color:white')
+        validSenha = true
+
+    }
+})
+
+confirmeSenha.addEventListener('keyup', () => {
+    if(senha.value != confirmeSenha.value) {
+        labelConfirmeSenha.setAttribute('style', 'color: red')
+        labelConfirmeSenha.innerHTML = ' Confirmar Senha: Senha Diferente '
+        confirmeSenha.setAttribute('style', 'border-color:red')
+        validConfirmeSenha = false
+
+    }else{
+        labelConfirmeSenha.setAttribute('style', 'color: white')
+        labelConfirmeSenha.innerHTML = 'Confirmar Senha'
+        confirmeSenha.setAttribute('style', 'border-color:white')
+        validConfirmeSenha = true
+
+    }
+})
+
+
+function cadastrar(){
+    if(validNome && validUsuario && validSenha && validConfirmeSenha){
+        alert('Tudo ok pae')
+    }
+    else{
+        alert('Ta tudo errado')
+    }
+}
+
 btn.addEventListener('click', () => {
     let inputSenha = document.querySelector('#senha',)
 
@@ -21,3 +112,4 @@ btnC.addEventListener('click', () => {
         inputConfirmeSenha.setAttribute('type' , 'password')
     }
 }) 
+
